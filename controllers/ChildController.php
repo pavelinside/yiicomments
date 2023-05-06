c3e418154787ad44fe3840d4f14a7069ea3ae03a
pavelinside <7697522+pavelinside@users.noreply.github.com>
2023-05-07 00:07:04 +0300

    BaseController for login users

---
+++ controllers/ChildController.php
@@ -0,0 +1,39 @@
+<?php
+namespace app\controllers;
+
+use yii\helpers\ArrayHelper;
+use app\components\BaseController;
+
+class ChildController extends BaseController
+{
+    public function actions()
+    {
+        return ArrayHelper::merge(parent::actions(), [
+            'page' => [
+                'class' => 'yii\web\ViewAction',
+            ],
+        ]);
+    }
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $rules = $behaviors['access']['rules'];
+        $rules = ArrayHelper::merge(
+            $rules,
+            [
+                [
+                    'allow' => true,
+                    'actions' => ['page']
+                ]
+            ]
+        );
+        $behaviors['access']['rules'] = $rules;
+        return $behaviors;
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
