b8b3d0fa9110f316496499cf9ae8438592027f79
pavelinside <7697522+pavelinside@users.noreply.github.com>
2023-05-07 13:26:23 +0300

    Using ActionFilter

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -5,9 +5,19 @@ namespace app\controllers;
 use Yii;
 use yii\helpers\Html;
 use yii\web\Controller;
+use app\components\CustomFilter;
 
 class TestController extends Controller
 {
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => CustomFilter::className(),
+            ],
+        ];
+    }
+
     public function actionIndex()
     {
         return $this->renderContent(Html::tag('h2',
